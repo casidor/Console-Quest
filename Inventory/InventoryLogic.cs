6827b9a1932306689d1ddf955f9162d3e6dc51f0
casidor <rvolosin0@gmail.com>
2026-03-23 17:36:11 +0200

    Implemented InventoryLogic
    - Added UseItem with IUsable check and inventory removal
    - Added GetUsableInBattle filter for battle item selection

--- Inventory/InventoryLogic.cs
+++ Inventory/InventoryLogic.cs
@@ -1,5 +1,28 @@
+using Game;
 using Items;
 namespace Inventory
 {
-    
+    class InventoryLogic
+    {
+        public bool UseItem(Hero hero, Inventory inventory, ItemStack stack)
+        {
+            if( stack.Item is IUsable usable)
+            {
+                var result = usable.Use(hero);
+                if (result)
+                {
+                    return inventory.Remove(stack.Item, 1);  
+                }
+                return false;
+            }
+            return false;
+        }
+        public IEnumerable<ItemStack> GetUsableInBattle(Inventory inventory)
+        {
+            foreach( var stack in inventory.GetAll())
+            {
+                if(stack.Item is HealItem) yield return stack;
+            }
+        }
+    }
 }
